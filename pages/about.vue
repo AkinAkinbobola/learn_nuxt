8a58cd1b028ad876eef6ddd20477d6f8c9bf56df
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-30 12:53:09 +0100

    Updates

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1 class="font-bold">About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam illum incidunt ipsa nostrum officia pariatur
+      provident quis saepe sed ut.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
