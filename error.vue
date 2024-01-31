94cfe95f7cdd9c358d5d632d93b1be892584b6aa
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-31 12:00:43 +0100

    Error redirect

--- error.vue
+++ error.vue
@@ -1,12 +1,14 @@
 <script setup>
 defineProps(["error"])
+const handleClearError = () => clearError({redirect: "/"})
 </script>
 
 <template>
   <div class="mt-7 max-w-sm mx-auto text-center card">
     <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Oops.</p>
-    <p class="mt-7">{{error.message}}</p>
+    <p class="mt-7">{{ error.message }}</p>
+    <button class="btn" @click="handleClearError">Go home</button>
   </div>
 </template>
 
