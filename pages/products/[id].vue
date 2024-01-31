0d001cb48265cc6a1bb0ae6e5c6ffa9a671f5d1f
Akin Akinbobola <akinbobola71@gmail.com>
2024-01-31 11:37:26 +0100

    Style products page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-const {id} = useRoute().params
 definePageMeta({
   layout: "products"
 })
+const {id} = useRoute().params
+const uri = `https://fakestoreapi.com/products/${id}`;
+const {data: product} = await useFetch(uri);
 </script>
 
 <template>
   <div>
-    <p>Product details for product {{ id }}</p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
